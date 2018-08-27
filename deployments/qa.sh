cc55deca72964814d979f93225c8d7bbd3ef9397
marcosvissio <marcos.vissioQgmail.com>
2018-08-26 23:57:13 -0300

    cambio por npm

--- deployments/qa.sh
+++ deployments/qa.sh
@@ -1,6 +1,6 @@
 npm install -g now
 echo "deploying..."
 echo $NOW_TOKEN
-URL=$(now --npm -t $NOW_TOKEN)
+URL=$(now --npm -t $NOW_TOKEN --public)
 echo "running acceptance on $URL"
 curl --silent -L $URL
